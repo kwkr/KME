976de00ce9ed04616218efd261d12db3e354359d
kwkr <kawka.maciej.93@gmail.com>
2023-11-22 18:34:51 +0100

    add backing up solutions

--- scripts/attempt.mjs
+++ scripts/attempt.mjs
@@ -6,7 +6,8 @@ import { formatDate, loadStats, saveStats } from './utils.mjs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const statsPath = path.join(__dirname, "..", "stats.json");
+const backupPath = path.join(__dirname, "..", "backup");
+const exercisePath = path.join(__dirname, "..", "today", 'index.ts');
 
 try {
     execSync("npm test", { stdio: 'inherit' });
@@ -20,17 +21,26 @@ if (testRunData.numFailedTestSuites > 0) {
     // tests passed
     const statsData = loadStats();
     const now = new Date();
-    statsData.current.end = now.toISOString();
-    const key = formatDate(now);
-    if (!statsData.history) {
-        statsData.history = {};
+    if (statsData.current) {
+        const exerciseName = statsData.current.name;
+        statsData.current.end = now.toISOString();
+
+        const key = formatDate(now);
+        if (!statsData.history) {
+            statsData.history = {};
+        }
+        if (!statsData.history[key]) {
+            statsData.history[key] = [];
+        }
+        statsData.history[key].push(statsData.current);
+
+        if (!fs.existsSync(backupPath)) {
+            fs.mkdirSync(backupPath)
+        }
+        fs.copyFileSync(exercisePath, `${backupPath}/${exerciseName}-${now.getTime()}.ts`);
+        delete statsData.current;
+        saveStats(statsData);
     }
-    if (!statsData.history[key]) {
-        statsData.history[key] = [];
-    }
-    statsData.history[key].push(statsData.current);
-    delete statsData.current;
-    saveStats(statsData);
 }
 
 fs.rmSync(tmpPath);
